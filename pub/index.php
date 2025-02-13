83a5f9bd855468d00c0792a9e57d34b9964736fc
sahab-fortune4 <sahab.gautam@fortune4.in>
2025-02-13 10:41:51 +0530

    update the style of the filter

--- pub/index.php
+++ pub/index.php
@@ -6,6 +6,64 @@
  * See COPYING.txt for license details.
  */
 
+
+
+    function getMetalPrices($apiKey, $currency = 'INR', $unit = 'g') {
+        // API URL
+        $url = "https://api.metals.dev/v1/latest?api_key=" . $apiKey . "&currency=" . $currency . "&unit=" . $unit;
+    
+        // Initialize cURL session
+        $curl = curl_init();
+    
+        // Set cURL options
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        
+        $headers = array("Accept: application/json");
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        
+        $response = curl_exec($curl);
+    
+        // Check if there was an error in the cURL request
+        if(curl_errno($curl)) {
+            echo 'Error:' . curl_error($curl);
+        }
+    
+        // Close cURL session
+        curl_close($curl);
+    
+        // Decode the JSON response
+        $data = json_decode($response, true);
+
+        // Check if the data is valid
+        if (isset($data['status']) && $data['status'] === 'success') {
+            // return $data['metals'];
+            return $data;
+
+        } else {
+            // Return an error message if the API response is invalid
+            echo 'Error: Unable to fetch data from the API.';
+        }
+    }
+ 
+    // Usage example:
+    // $apiKey = 'INKIZKIOTWCK7JDGCPXR422DGCPXR'; // Replace with your actual API key
+    // $currency = 'INR'; // Example: INR or USD
+    // $unit = 'g'; // Example: g, kg
+    
+    // $prices = getMetalPrices($apiKey, $currency, $unit);
+    
+    // if (is_array($prices)) {
+    //     echo "Metal Rate<pre>";
+    //     print_r($prices); // Prints the metal prices data
+    //     echo "</pre>";
+    // } else {
+    //     // Display error message
+    //     echo $prices;
+    // }
+ 
+ 
+    // die;
 use Magento\Framework\App\Bootstrap;
 
 try {
